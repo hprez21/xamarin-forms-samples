528f49d8c5994994bf0f2a11c5fbdc3dc13331f5
Héctor Uriel Pérez Rojas <hectorperez_11c@cenidet.edu.mx>
2016-04-20 12:09:19 -0500

    First commit and Model added
    First commit and Model added

---
+++ JsonWebServices/JsonWebServices/JsonWebServices/Model/WeatherObservation.cs
@@ -0,0 +1,30 @@
+﻿namespace JsonWebServices.Model
+{
+
+    public class WeatherResult
+    {
+        public Weatherobservation WeatherObservation { get; set; }
+    }
+
+    public class Weatherobservation
+    {
+        public int Elevation { get; set; }
+        public float Lng { get; set; }
+        public string Observation { get; set; }
+        public string Icao { get; set; }
+        public string Clouds { get; set; }
+        public string DewPoint { get; set; }
+        public string CloudsCode { get; set; }
+        public string Datetime { get; set; }
+        public string CountryCode { get; set; }
+        public string Temperature { get; set; }
+        public int Humidity { get; set; }
+        public string StationName { get; set; }
+        public string WeatherCondition { get; set; }
+        public int WindDirection { get; set; }
+        public int HectoPascAltimeter { get; set; }
+        public string WindSpeed { get; set; }
+        public float Lat { get; set; }
+    }
+
+}
